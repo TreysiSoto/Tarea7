b8c336da83e91a605582481698873d021ca89b29
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-05-16 18:00:16 -0500

    Create MainMulti.java

---
+++ MainMulti.java
@@ -0,0 +1,12 @@
+package Clase007;
+
+public class Main {
+    public static void main(String[] args) {
+        MultMatriz producto = new MultMatriz();
+        producto.agregarMatriz1();
+        producto.agregarMatriz2();
+        producto.calcularMultiplicacion();
+
+    }
+
+}
