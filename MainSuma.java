d8f490242a8bb62d50533807a53b960d1b2b0bd8
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-05-16 17:59:21 -0500

    Create MainSuma.java

---
+++ MainSuma.java
@@ -0,0 +1,10 @@
+package Clase07;
+
+public class Main {
+    public static void main(String[] args) {
+        SumaMatriz sumares = new SumaMatriz();
+        sumares.ingresarMatrizA();
+        sumares.ingresarMatrizB();
+        sumares.calcularSuma();
+    }
+}
